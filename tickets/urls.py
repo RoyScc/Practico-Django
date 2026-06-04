19a0806504cb6bfc8b688c435179643fb9c582f1
RoyScc <r.scheurer@itecriocuarto.org.ar>
2026-06-04 09:35:00 -0300

    funcionalidades y users

--- tickets/urls.py
+++ tickets/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     #path('', views.index, name="index"),
-    path('', views.cine, name="cine"),
+    path('', views.login_view, name="login"),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
